6b9b8d085e721cbf0f56c8520691c30dc42ee330
PabloGomez10 <102038179+PabloGomez10@users.noreply.github.com>
2023-12-30 23:57:41 -0300

    .
    .

---
+++ server/data/fs/users.fs.js
@@ -0,0 +1,82 @@
+import fs from fs;
+import path from path;
+import crypto from crypto;
+
+class UserManager {
+    constructor() {
+        this.filePath = path.join(__dirname, 'data', 'users.json');
+        this.loadUsers();
+    }
+
+    create(data) {
+        const user = {
+            id: this.generateId(),
+            name: data.name,
+            photo: data.photo,
+            email: data.email
+        };
+
+        this.users.push(user);
+        this.saveUsers();
+        return user;
+    }
+
+    read() {
+        return this.users;
+    }
+
+    readOne(id) {
+        return this.users.find(user => user.id === id);
+    }
+
+    loadUsers() {
+        try {
+            const data = fs.readFileSync(this.filePath, 'utf8');
+            this.users = JSON.parse(data);
+        } catch (error) {
+            this.users = [];
+        }
+    }
+
+    saveUsers() {
+        try {
+            const data = JSON.stringify(this.users, null, 2);
+            fs.writeFileSync(this.filePath, data, 'utf8');
+        } catch (error) {
+            console.log('Error saving users to file:', error.message);
+        }
+    }
+
+    generateId() {
+        const id = crypto.randomBytes(6).toString('hex'); 
+        return id;
+    }
+}
+
+const dataFolderPath = path.join(__dirname, 'data');
+if (!fs.existsSync(dataFolderPath)) {
+    fs.mkdirSync(dataFolderPath);
+}
+
+const productManager = new ProductManager();
+const userManager = new UserManager();
+
+const product1 = productManager.create({
+    title: 'Bomba de agua',
+    photo: 'link',
+    price: 34000,
+    stock: 10
+});
+
+const user1 = userManager.create({
+    name: 'PABLO GOMEZ',
+    photo: 'foto no',
+    email: 'pablo123@gmail.com'
+});
+
+console.log('Productos:', productManager.read());
+console.log('Usuarios:', userManager.read());
+console.log('Producto específico:', productManager.readOne(product1.id));
+console.log('Usuario específico:', userManager.readOne(user1.id));
+
+export default UserManager;
