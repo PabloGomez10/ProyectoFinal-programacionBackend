14faf0b212c98a6a13eb87c20495628fae10c3c9
PabloGomez10 <102038179+PabloGomez10@users.noreply.github.com>
2023-12-31 18:36:54 -0300

    .
    .

--- server/data/fs/products.fs.js
+++ server/data/fs/products.fs.js
@@ -1,33 +1,52 @@
-import fs from fs;
-import path from path;
-import crypto from crypto;
+
+import fs from "fs";
+import path from "path";
+import crypto from "crypto";
 
 class ProductManager {
     constructor() {
-        this.filePath = path.join(__dirname, 'data', 'products.json');
+        const __filename = new URL(import.meta.url).pathname;
+        this.filePath = path.join(path.dirname(__filename), 'files/products.json');
+
+        this.products = [];
         this.loadProducts();
     }
 
+    generateId() {
+        const id = crypto.randomBytes(6).toString('hex');
+        return id;
+    }
+
     create(data) {
-        const product = {
+        const newProduct = {
             id: this.generateId(),
-            title: data.title,
-            photo: data.photo,
-            price: data.price,
-            stock: data.stock
+            ...data,
         };
 
-        this.products.push(product);
+        this.products.push(newProduct);
         this.saveProducts();
-        return product;
+        return newProduct;
     }
 
     read() {
+        console.log("Reading all products:", this.products);
         return this.products;
     }
 
     readOne(id) {
+        console.log("Reading product with ID:", id);
         return this.products.find(product => product.id === id);
+    }
+
+    destroy(id) {
+        const index = this.products.findIndex(product => product.id === id);
+        if (index !== -1) {
+            this.products.splice(index, 1);
+            this.saveProducts();
+            return true; // Éxito en la eliminación
+        } else {
+            return false; // No se encontró el producto
+        }
     }
 
     loadProducts() {
@@ -35,6 +54,7 @@ class ProductManager {
             const data = fs.readFileSync(this.filePath, 'utf8');
             this.products = JSON.parse(data);
         } catch (error) {
+            console.error('Error loading products:', error.message);
             this.products = [];
         }
     }
@@ -47,11 +67,67 @@ class ProductManager {
             console.log('Error saving products to file:', error.message);
         }
     }
-
-    generateId() {
-        const id = crypto.randomBytes(6).toString('hex'); 
-        return id;
-    }
 }
 
 export default ProductManager;
+
+
+
+// <------------------------------------>
+// import fs from 'fs';
+// import path from 'path';
+// import crypto from 'crypto';
+
+// class ProductManager {
+//     constructor() {
+//         this.filePath = path.join(__dirname, 'data', 'products.json');
+//         this.loadProducts();
+//     }
+
+//     create(data) {
+//         const product = {
+//             id: this.generateId(),
+//             title: data.title,
+//             photo: data.photo,
+//             price: data.price,
+//             stock: data.stock
+//         };
+
+//         this.products.push(product);
+//         this.saveProducts();
+//         return product;
+//     }
+
+//     read() {
+//         return this.products;
+//     }
+
+//     readOne(id) {
+//         return this.products.find(product => product.id === id);
+//     }
+
+//     loadProducts() {
+//         try {
+//             const data = fs.readFileSync(this.filePath, 'utf8');
+//             this.products = JSON.parse(data);
+//         } catch (error) {
+//             this.products = [];
+//         }
+//     }
+
+//     saveProducts() {
+//         try {
+//             const data = JSON.stringify(this.products, null, 2);
+//             fs.writeFileSync(this.filePath, data, 'utf8');
+//         } catch (error) {
+//             console.log('Error saving products to file:', error.message);
+//         }
+//     }
+
+//     generateId() {
+//         const id = crypto.randomBytes(6).toString('hex'); 
+//         return id;
+//     }
+// }
+
+// export default ProductManager;
--- server/data/fs/users.fs.js
+++ server/data/fs/users.fs.js
@@ -1,32 +1,51 @@
-import fs from fs;
-import path from path;
-import crypto from crypto;
+import fs from "fs";
+import path from "path";
+import crypto from "crypto";
 
 class UserManager {
     constructor() {
-        this.filePath = path.join(__dirname, 'data', 'users.json');
+        const __filename = new URL(import.meta.url).pathname;
+        this.filePath = path.join(path.dirname(__filename), 'files/users.json');
+
+        this.users = [];
         this.loadUsers();
     }
 
+    generateId() {
+        const id = crypto.randomBytes(6).toString('hex');
+        return id;
+    }
+
     create(data) {
-        const user = {
+        const newUser = {
             id: this.generateId(),
-            name: data.name,
-            photo: data.photo,
-            email: data.email
+            ...data,
         };
 
-        this.users.push(user);
+        this.users.push(newUser);
         this.saveUsers();
-        return user;
+        return newUser;
     }
 
     read() {
+        console.log("Reading all users:", this.users);
         return this.users;
     }
 
     readOne(id) {
+        console.log("Reading user with ID:", id);
         return this.users.find(user => user.id === id);
+    }
+
+    destroy(id) {
+        const index = this.users.findIndex(user => user.id === id);
+        if (index !== -1) {
+            this.users.splice(index, 1);
+            this.saveUsers();
+            return true; // Éxito en la eliminación
+        } else {
+            return false; // No se encontró el usuario
+        }
     }
 
     loadUsers() {
@@ -34,6 +53,7 @@ class UserManager {
             const data = fs.readFileSync(this.filePath, 'utf8');
             this.users = JSON.parse(data);
         } catch (error) {
+            console.error('Error loading users:', error.message);
             this.users = [];
         }
     }
@@ -46,37 +66,92 @@ class UserManager {
             console.log('Error saving users to file:', error.message);
         }
     }
-
-    generateId() {
-        const id = crypto.randomBytes(6).toString('hex'); 
-        return id;
-    }
 }
-
-const dataFolderPath = path.join(__dirname, 'data');
-if (!fs.existsSync(dataFolderPath)) {
-    fs.mkdirSync(dataFolderPath);
-}
-
-const productManager = new ProductManager();
-const userManager = new UserManager();
-
-const product1 = productManager.create({
-    title: 'Bomba de agua',
-    photo: 'link',
-    price: 34000,
-    stock: 10
-});
-
-const user1 = userManager.create({
-    name: 'PABLO GOMEZ',
-    photo: 'foto no',
-    email: 'pablo123@gmail.com'
-});
-
-console.log('Productos:', productManager.read());
-console.log('Usuarios:', userManager.read());
-console.log('Producto específico:', productManager.readOne(product1.id));
-console.log('Usuario específico:', userManager.readOne(user1.id));
 
 export default UserManager;
+
+
+// <-------------------------------------------------------->
+// import fs from 'fs';
+// import path from 'path';
+// import crypto from 'crypto';
+
+
+// class UserManager {
+//     constructor() {
+//         this.filePath = path.join(__dirname, 'data', 'users.json');
+//         this.loadUsers();
+//     }
+
+//     create(data) {
+//         const user = {
+//             id: this.generateId(),
+//             name: data.name,
+//             photo: data.photo,
+//             email: data.email
+//         };
+
+//         this.users.push(user);
+//         this.saveUsers();
+//         return user;
+//     }
+
+//     read() {
+//         return this.users;
+//     }
+
+//     readOne(id) {
+//         return this.users.find(user => user.id === id);
+//     }
+
+//     loadUsers() {
+//         try {
+//             const data = fs.readFileSync(this.filePath, 'utf8');
+//             this.users = JSON.parse(data);
+//         } catch (error) {
+//             this.users = [];
+//         }
+//     }
+
+//     saveUsers() {
+//         try {
+//             const data = JSON.stringify(this.users, null, 2);
+//             fs.writeFileSync(this.filePath, data, 'utf8');
+//         } catch (error) {
+//             console.log('Error saving users to file:', error.message);
+//         }
+//     }
+
+//     generateId() {
+//         const id = crypto.randomBytes(6).toString('hex'); 
+//         return id;
+//     }
+// }
+
+// const dataFolderPath = path.join(__dirname, 'data');
+// if (!fs.existsSync(dataFolderPath)) {
+//     fs.mkdirSync(dataFolderPath);
+// }
+
+// const productManager = new ProductManager();
+// const userManager = new UserManager();
+
+// const product1 = productManager.create({
+//     title: 'Bomba de agua',
+//     photo: 'link',
+//     price: 34000,
+//     stock: 10
+// });
+
+// const user1 = userManager.create({
+//     name: 'PABLO GOMEZ',
+//     photo: 'foto no',
+//     email: 'pablo123@gmail.com'
+// });
+
+// console.log('Productos:', productManager.read());
+// console.log('Usuarios:', userManager.read());
+// console.log('Producto específico:', productManager.readOne(product1.id));
+// console.log('Usuario específico:', userManager.readOne(user1.id));
+
+// export default UserManager;
