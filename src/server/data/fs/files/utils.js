90e3a9697c7b8e7141bc0e5c53ec358c8b1856b4
PabloGomez10 <102038179+PabloGomez10@users.noreply.github.com>
2024-01-16 23:52:07 -0300

    .
    .

---
+++ src/server/data/fs/files/utils.js
@@ -0,0 +1,7 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+ 
+export const getDirname = importMeta => {
+  const __filename = fileURLToPath(importMeta.url);
+  return dirname(__filename);
+};
